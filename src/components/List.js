51c48625f8f1c52eb51deedcb45d73962ae8ef49
Sunny Patel <spp1991@gmail.com>
2020-02-29 14:47:06 -0500

    Added axios and getNotes method

---
+++ src/components/List.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class List extends React.Component {
+
+    componentWillMount(){
+        this.props.getNotes();
+    }
+
+    render(){
+        return(
+            <div className="list-container">
+                List Component
+            </div>
+        );
+
+    }
+}
+
+export default List;
