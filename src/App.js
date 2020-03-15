1458790c9da512da02cc4849c0e72af891a5abb0
Sunny Patel <spp1991@gmail.com>
2020-03-14 22:44:23 -0400

    Updated code to update existing note

--- src/App.js
+++ src/App.js
@@ -35,8 +35,18 @@ getNote = (id) => {
   .catch((err) => console.log(err.response.data))
 }
 
-submitNote = (data) => {
-  axios.post(urlFor('notes'),data)
+performSubmissionRequest= (data,id) => {
+  if(id){
+    //TODO: perform update request
+    return axios.patch(urlFor(`notes/${id}`),data)
+  }
+  else{
+    return axios.post(urlFor("notes"),data);
+  }
+  }
+
+submitNote = (data,id) => {
+  this.performSubmissionRequest(data,id)
   .then((res) => this.setState({showNote:false}))
   .catch((err) => console.log(err.response.data));
 
